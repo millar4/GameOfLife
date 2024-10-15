ef9b120550a7d5532b7795a1336d2654f8e0adf2
Millar Scandrett <millarscandrett@eduroam-13-54.nomadic.bris.ac.uk>
2024-10-15 16:14:00 +0100

    Initial game oflife implementation

--- gol-skeleton-master/gol/gol.go
+++ gol-skeleton-master/gol/gol.go
@@ -8,6 +8,51 @@ type Params struct {
 	ImageHeight int
 }
 
+func calculateNextState(p Params, world [][]byte) [][]byte {
+	worldUpdate := make([][]byte, p.ImageHeight)
+	for i := range worldUpdate {
+		worldUpdate[i] = make([]byte, p.ImageWidth)
+	}
+
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			sum := int(world[(y+p.ImageHeight-1)%p.ImageHeight][(x+p.ImageWidth-1)%p.ImageWidth])/255 + int(world[(y+p.ImageHeight-1)%p.ImageHeight][(x+p.ImageWidth)%p.ImageWidth])/255 + int(world[(y+p.ImageHeight-1)%p.ImageHeight][(x+p.ImageWidth+1)%p.ImageWidth])/255 +
+				int(world[(y+p.ImageHeight)%p.ImageHeight][(x+p.ImageWidth-1)%p.ImageWidth])/255 + int(world[(y+p.ImageHeight)%p.ImageHeight][(x+p.ImageWidth+1)%p.ImageWidth])/255 +
+				int(world[(y+p.ImageHeight+1)%p.ImageHeight][(x+p.ImageWidth-1)%p.ImageWidth])/255 + int(world[(y+p.ImageHeight+1)%p.ImageHeight][(x+p.ImageWidth)%p.ImageWidth])/255 + int(world[(y+p.ImageHeight+1)%p.ImageHeight][(x+p.ImageWidth+1)%p.ImageWidth])/255
+			//fmt.Println(world)
+			if world[y][x] == 255 {
+				if sum < 2 {
+					worldUpdate[y][x] = 0
+				} else if sum == 2 || sum == 3 {
+					worldUpdate[y][x] = 255
+				} else {
+					worldUpdate[y][x] = 0
+				}
+			} else {
+				if sum == 3 {
+					worldUpdate[y][x] = 255
+				} else {
+					worldUpdate[y][x] = 0
+				}
+			}
+		}
+	}
+	//fmt.Println(worldUpdate)
+	return worldUpdate
+}
+
+func calculateAliveCells(p Params, world [][]byte) []cell {
+	var liveCells []cell
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			if world[y][x] == 255 {
+				liveCells = append(liveCells, cell{x: x, y: y})
+			}
+		}
+	}
+	return liveCells
+}
+
 // Run starts the processing of Game of Life. It should initialise channels and goroutines.
 func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 
